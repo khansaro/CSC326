8cb34d97d3864ed57699d9e0376984c1f0c1680a
Kadam  Mohammed Anas Imtiaz <kadammo1@ug140.eecg>
2015-12-05 00:49:54 -0500

    Test

--- Lab3-front/install_script.sh
+++ Lab3-front/install_script.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade
-apt-get install -y build-essential
-apt-get install -y python-dev
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y > /dev/null
+apt-get upgrade -y  > /dev/null
+apt-get install -y build-essential  > /dev/null
+apt-get install -y python-dev  > /dev/null
 cd bottle-0.12.7
-python setup.py install
+python setup.py install  > /dev/null
 cd ../httplib2-0.8
-python setup.py install
+python setup.py install  > /dev/null
 cd ../setuptools-18.7.1
-python setup.py install
+python setup.py install  > /dev/null
 cd ../Beaker-1.7.0
-python setup.py install
+python setup.py install  > /dev/null
 cd ..
-tar -xvf numpy-1.10.1.tar.gz
+tar -xvf numpy-1.10.1.tar.gz  > /dev/null
 cd numpy-1.10.1
-python setup.py install
+python setup.py install  > /dev/null
 cd ..
 echo "Search engine is available on port 8080 at:"
 dig +short myip.opendns.com @resolver1.opendns.com
